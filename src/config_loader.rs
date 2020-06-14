bfd02893bbe479fbbccd45b3f7daa977a1669168
l1npengtul <l1npengtul@protonmail.com>
2020-06-15 07:52:30 +0900

    defined cfg struct and impl

--- src/config_loader.rs
+++ src/config_loader.rs
@@ -13,13 +13,38 @@ pub struct BotConfig{
     discord_api : String,
     detailed_network : bool,
     detailed_debug : bool,
-    banned_words : Vector<String>,
-    banned_links : Vector<String>
+    banned_words_global : Vector<String>,
+    banned_links_global : Vector<String>
 }
 impl BotConfig {
     pub fn get_discord_api(&self) -> &String{
         &self.discord_api
     }
+    pub async fn is_network(&self)->&bool{
+        &self.detailed_network
+    }
+    pub async fn is_debug(&self)->&bool{
+        &self.detailed_debug
+    }
+    pub async fn contains_word(&self, word : &str) -> &bool{
+        if &self.banned_words_global.contains(word){
+            return &true;
+        }
+        else{
+            return &false;
+        }
+        &false
+    }
+    pub async fn contains_link(&self, link : &str) -> &bool{
+        if &self.banned_links_global.contains(link){
+            return &true;
+        }
+        else{
+            return &false;
+        }
+        &false
+    }
+
 }
 
 
@@ -28,21 +53,6 @@ impl BotConfig {
 #[derive(Deserialize)]
 struct
 
-pub async fn get_query(host : &str, usr : &str, query : &str, param : &str) -> Result<Vec<Row>, Error>{
-    // Connect to PostgreSQL
-    let (client, connection) = tokio_postgres::connect(format!("host={} user={}",host,usr).as_str(), NoTls).await?;
-
-    // Spawn the tokio thread since the connection does its own thing and talks to the DB
-    tokio::spawn(async move {
-        if let Err(e) = connection.await{
-            eprintln!("PostgreSQL Connection Error: {}", e);
-        }
-    });
-
-    let rows = client.query(query, &[&param]).await?;
-
-    Ok(rows)
-}
 
 // File write example
 /*
@@ -108,14 +118,44 @@ pub fn get_config() -> Option<BotConfig>{
         Err(why)=> {
             eprintln!("Error: No bot.toml file found. It has been generated for you, please fill it out!");
             let mut create_file = match File::create("bot.toml"){
-                Ok(a) => a,
+                Ok(mut a) => {
+                    a.write_all(r#"
+title="bot config file"
+
+# hashtags indicate line comments
+# go to https://discord.com/developers/applications and make a bot
+# don't share this key with anyone but yourself
+# replace <API_KEY> with the api key you got from discord
+[discord]
+discord_api=<API_KEY>
+
+[preferences]
+# true  => Network Congestion, Download/Upload Speed, Packet Loss, Ping
+# false => ping
+detailed_network=true
+# true  => Program RAM usage, CPU usage, Threads
+detailed_debug=true
+
+[globals]
+# banned search terms
+# lists go like ["term1", "term2"]
+banned_search=[]
+# note: use links
+# this is good: ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=F5oQoNMpqi8"]
+# this is no good: ["F5oQoNMpqi8", "dQw4w9WgXcQ", "hentai"]
+banned_links=[]"#.as_ref()).unwrap();
+                    panic!("bot.toml generated");
+                },
                 Err(why) => {
                     panic!("Error! Could not create bot.toml!");
                 }
             };
 
 
+
         }
 
     };
+
+
 }
